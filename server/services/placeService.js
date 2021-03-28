30b214c7a60222213226462fc0d8e56ddd29e555
AnnieBunny <anyyy02@abv.bg>
2021-03-28 16:25:16 +0300

    add new functions
    create,delete , getOne

--- server/services/placeService.js
+++ server/services/placeService.js
@@ -1,43 +1,36 @@
 const Place = require('../models/Place');
 const User = require('../models/User');
 
-
-
-
-function create(data, userId) {
-    let place = new ({ ...data, creator: userId });
-
+async function create(data, userId) {
+    let place = await new Place({ ...data, creator: userId })
     return place.save();
-};
+   
+    
+}
 
-// function getAll() {
-//     let expenses = Expense.find({}).lean();
+async function getAll(userId) {
+    return await Place.find({ creator: userId }).lean();
+}
 
-//     return expenses;
-// };
+async function getSpecific(id, userId) {
+    let place = await Place.findById(id).lean();
+    return place;
+}
 
+async function update(id, data) {
+    return await Place.updateOne({ _id: id }, data);
+}
 
-
-function getOne(id) {
-
-    return Place.findById(id).lean()
-        .then(place => {
-            return place;
-        })
-
-};
-function deleteOne(placeId) {
-    return Place.deleteOne({ _id: placeId });
-};
-
-
-
-
+async function deletePlace(id, userId) {
+    await Place.deleteOne({ _id: id });
+    await User.updateOne({ _id: userId }, { $pull: { createdSells: id } });
+    return;
+}
 
 module.exports = {
     create,
-    // getAll,
-    getOne,
-    deleteOne,
-   
+    getAll,
+    getSpecific,
+    update,
+    deletePlace,
 }
