24c7db5b19df685468b0dafe60fff3d0b9b47b08
AnnieBunny <anyyy02@abv.bg>
2021-03-28 15:33:11 +0300

    update auth
    update some functions

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,22 +2,24 @@
 const jwt = require('jsonwebtoken');
 const {COOKIE_NAME, SECRET} = require('../config/config')
 
-const authenticated = () => {
-    return (req, res, next) => {
-        let token = req.cookies[COOKIE_NAME];
+function auth(req, res, next) {
 
-        if (token) {
-            jwt.verify(token, SECRET, (err, decoded) => {
-                if (err) {
-                    res.clearCookie(COOKIE_NAME);
-                } else {
-                    req.user = decoded;
-                    req.user.isAuthenticated = true;
-                }
-            })
-        }
-        next();
-    }
-}
+    let token = req.cookies[COOKIE_NAME];
+    console.log(token)
 
-module.exports = authenticated;
+    if(token) {
+        jwt.verify(token, SECRET, function (err, decoded) {
+            if(err) {
+                res.clearCookie(COOKIE_NAME)
+            } else {
+                req.user = decoded;
+                res.locals.user= decoded;
+                res.locals.isAuth = true;
+            };
+        });
+    };
+    next();
+};
+
+
+module.exports = auth;
