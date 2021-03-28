16bcc41f01247a899c4a58d22f5911f16a6c4e1a
AnnieBunny <anyyy02@abv.bg>
2021-03-28 16:18:12 +0300

    change login and register functions

--- server/services/authService.js
+++ server/services/authService.js
@@ -3,43 +3,26 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { SECRET } = require('../config/config');
 
-const register = async (username, password) => {
+async function registerUser(email, password, amount) {
+    let checkUser = await User.findOne({ email });
+    if (checkUser) throw { message: 'These email is already taken.' };
 
+    let user = new User({ email, password });
+    return await user.save();
+}
 
-    if (password !== repeatPassword) throw {message: 'Passwords should match.'};
+async function loginUser({ email, password }) {
+    let user = await User.findOne({ email });
+    if (!user) throw { message: 'Invalid email or password' };
 
-        if (password.length < 4) throw {message: 'Password should be at least 4 characters long.'};
+    let hasValidPass = await bcrypt.compare(password, user.password);
+    if (!hasValidPass) throw { message: "Invalid email or password" }
 
-        if (username.length < 4) throw {
-            message: 'Username should be at least 4 characters long and should contains only english letters and digits'
-        };
-
-        let isAlreadyExists = await User.findOne({username});
-
-        if (isAlreadyExists) throw {message: 'User already exists!'};
-
-        let hash = await bcrypt.hash(password, SALT_ROUNDS);
-
-        let user = await new User({username, password: hash}).save();
-
-        return jwt.sign({_id: user._id, username: user.username}, SECRET);
-    
-};
-
-const login = async (username, password) => {
-
-    let user = await User.findOne({ username });
-    if (!user) throw { message: 'No such user', status: 404 };
-
-    let areEqual = await bcrypt.compare(password, user.password);
-    if (!areEqual) throw { message: 'Invalid password', status: 404 };
-   
-    let token = jwt.sign({_id: user._id, username: user.username}, SECRET);
+    let token = jwt.sign({ _id: user._id, email: user.email }, SECRET, { expiresIn: '1h' });
     return token;
-};
+}
 
 module.exports = {
-    register,
-    login,
-
-};
+    registerUser,
+    loginUser
+}
