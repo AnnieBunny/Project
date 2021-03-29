9a3cbd3888cd52dd1e525c082c8b23ec56198347
AnnieBunny <anyyy02@abv.bg>
2021-03-29 17:45:24 +0300

    create auth service

---
+++ client/src/services/authService.js
@@ -0,0 +1,35 @@
+const url = 'http://localhost:4000';
+
+export const register = (email, password,repeatPassword) => {
+
+    let registerUser = {
+        email,
+        password,
+        repeatPassword
+    };
+
+   return fetch(`${url}/auth/register`, {
+        method: "POST",
+        headers:{ "Content-Type":"application/json"},
+        body: JSON.stringify(registerUser)
+    })
+
+
+};
+
+export const login = (email, password)=> {
+
+    let loginUser = {
+        email,
+        password,
+      
+    };
+
+   return fetch(`${url}/auth/login`, {
+        method: "POST",
+        headers:{ "Content-Type":"application/json"},
+        body: JSON.stringify(loginUser)
+    })
+
+};
+
