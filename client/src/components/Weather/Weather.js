9cc1f9b97360b97c20b74ae105115404359a9e13
AnnieBunny <anyyy02@abv.bg>
2021-04-01 17:16:16 +0300

    fix Weather html and css elements

--- client/src/components/Weather/Weather.js
+++ client/src/components/Weather/Weather.js
@@ -4,80 +4,28 @@ const Weather = () => {
     return (
         <div className={styles["user-home"]}>
             <div class={styles["container"]}>
-                <h2 id={styles["weather-title"]}>The weather is </h2>
-
+                <h2 id={styles["weather-title"]}>The weather today</h2>
             </div>
             <div className={styles["forecast-table"]}>
                 <div className={styles["container"]}>
                     <div className={styles["forecast-container"]}>
                         <div className={styles["today forecast"]}>
                             <div className={styles["forecast-header"]}>
-                               
                             </div>
 
                             <div className={styles["forecast-content"]}>
                                 <div className={styles["location"]}>New York</div>
-                                
+
                             </div>
                         </div>
+                        
+                      
                         <div className={styles["forecast"]}>
-                            <div className={styles["forecast-header"]}>
-                                <div className={styles["day"]}>Tuesday</div>
-                            </div>
+                         
                             <div className={styles["forecast-content"]}>
 
                                 <div className={styles["degree"]}>23<sup>o</sup>C</div>
-                                <small>18<sup>o</sup></small>
-                            </div>
-                        </div>
-                        <div className={styles["forecast"]}>
-                            <div className={styles["forecast-header"]}>
-                                <div className={styles["day"]}>Wednesday</div>
-                            </div>
-                            <div className={styles["forecast-content"]}>
-
-                                <div className={styles["degree"]}>23<sup>o</sup>C</div>
-                                <small>18<sup>o</sup></small>
-                            </div>
-                        </div>
-                        <div className={styles["forecast"]}>
-                            <div className={styles["forecast-header"]}>
-                                <div className={styles["day"]}>Thursday</div>
-                            </div>
-                            <div className={styles["forecast-content"]}>
-
-                                <div className={styles["degree"]}>23<sup>o</sup>C</div>
-                                <small>18<sup>o</sup></small>
-                            </div>
-                        </div>
-                        <div className={styles["forecast"]}>
-                            <div className={styles["forecast-header"]}>
-                                <div className={styles["day"]}>Friday</div>
-                            </div>
-                            <div className={styles["forecast-content"]}>
-
-                                <div className={styles["degree"]}>23<sup>o</sup>C</div>
-                                <small>18<sup>o</sup></small>
-                            </div>
-                        </div>
-                        <div className={styles["forecast"]}>
-                            <div className={styles["forecast-header"]}>
-                                <div className={styles["day"]}>Saturday</div>
-                            </div>
-                            <div className={styles["forecast-content"]}>
-
-                                <div className={styles["degree"]}>23<sup>o</sup>C</div>
-                                <small>18<sup>o</sup></small>
-                            </div>
-                        </div>
-                        <div className={styles["forecast"]}>
-                            <div className={styles["forecast-header"]}>
-                                <div className={styles["day"]}>Sunday</div>
-                            </div>
-                            <div className={styles["forecast-content"]}>
-
-                                <div className={styles["degree"]}>23<sup>o</sup>C</div>
-                                <small>18<sup>o</sup></small>
+                               
                             </div>
                         </div>
                     </div>
