57416b7b4d616d48616fe54faa7e94cdd4fea972
AnnieBunny <anyyy02@abv.bg>
2021-04-04 14:42:15 +0300

    add validation for edit form

--- client/src/components/EditPlace/EditPlace.js
+++ client/src/components/EditPlace/EditPlace.js
@@ -1,4 +1,5 @@
 import styles from './EditPlace.module.css'
+import InputError from '../InputError/InputError'
 
 import { useState, useEffect } from 'react'
 
@@ -15,16 +16,13 @@ const EditPlace = ({
     useEffect(() => {
         placeService.getOne(match.params.id)
             .then(res => {
-                setPlace(res)});
+                setPlace(res)
+            });
     }, [])
 
-console.log(place);
+
     const onDescriptionSaveSubmit = (e) => {
         e.preventDefault();
-      
-
-
-      
 
         let updatedPlace = {
             country: e.target.country.value,
@@ -32,27 +30,59 @@ console.log(place);
             imageUrl: e.target.imageUrl.value
         };
 
-     
+
 
         placeService.update(match.params.id, updatedPlace)
             .then((res) => {
-            
+
                 history.push(`/my-places`);
-               
+
                 return;
             });
     }
 
 
-    // const onDescriptionChangeHandler = (e) => {
-    //     console.log(e.target.value);
 
-    //     if (e.target.value.length < 10) {
-    //         setErrorMessage('Description too short');
-    //     } else {
-    //         setErrorMessage('');
-    //     }
-    // };
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const onDescriptionChangeHandler = (e) => {
+        if (e.target.value.length < 5) {
+            setErrorMessage('Description too short');
+        } else {
+            setErrorMessage('');
+        }
+    };
+
+
+    const [errorMessageForCity, setErrorMessageForCity] = useState('');
+
+    const onCityChangeHandler = (e) => {
+        let cityText = e.target.value;
+        let regex = /^[A-za-z]+/;
+        regex.test(cityText)
+
+        if (cityText) {
+            setErrorMessageForCity('Should be a letter and should start with upper character');
+        } else {
+            setErrorMessageForCity('');
+        }
+    };
+
+
+    const [errorMessageForImageUrl, setErrorMessageForImageUrl] = useState('');
+
+    const onImageUrlChangeHandler = (e) => {
+        let imageURL = e.target.value;
+        let regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+        regex.test(imageURL)
+
+        if (imageURL) {
+            setErrorMessageForImageUrl('Should be valid link adress');
+        } else {
+            setErrorMessageForImageUrl('');
+        }
+    };
 
     return (
         <div className={styles["backround-pic"]}>
@@ -61,18 +91,21 @@ console.log(place);
                     <form onSubmit={onDescriptionSaveSubmit}>
                         <label htmlFor="country">City</label>
                         <br />
-                        <input type="country" name="country" defaultValue={place.country} />
+                        <InputError>{errorMessageForCity}</InputError>
+                        <input type="country" name="country" defaultValue={place.country} onBlur={onCityChangeHandler}/>
                         <br />
-
                         <label htmlFor="description">Description</label>
                         <br />
+                        <InputError>{errorMessage}</InputError>
                         <textarea type="text"
-                            name="description" defaultValue={place.description}/>
+                            name="description" defaultValue={place.description} onBlur={onDescriptionChangeHandler} />
                         <br />
+                        <InputError>{errorMessageForImageUrl}</InputError>
 
                         <label htmlFor="imageUrl">ImageUrl </label>
                         <br />
-                        <input type="text" name="imageUrl" defaultValue={place.imageUrl}/>
+
+                        <input type="text" name="imageUrl" defaultValue={place.imageUrl} onBlur={onImageUrlChangeHandler} />
                         <br />
 
                         <input id="submitPlace" type="submit" value="Submit" />
